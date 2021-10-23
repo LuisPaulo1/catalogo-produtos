2a553273f784bba98a37bad47278403ece7d5f7f
LuisPaulo1 <csluispaulo@gmail.com>
2021-10-23 15:41:04 -0300

    update swagger

--- src/main/java/com/catalogoprodutos/config/SpringFoxConfig.java
+++ src/main/java/com/catalogoprodutos/config/SpringFoxConfig.java
@@ -6,17 +6,13 @@ import java.util.function.Consumer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.catalogoprodutos.controller.dto.ProductDTO;
 import com.catalogoprodutos.controller.exception.StandardError;
-import com.catalogoprodutos.controller.openapi.ProductControllerOpenAPI;
 import com.fasterxml.classmate.TypeResolver;
 
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -24,8 +20,6 @@ import springfox.documentation.builders.RepresentationBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.schema.AlternateTypeRule;
-import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
@@ -51,18 +45,9 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				.globalResponses(HttpMethod.GET, globalGetResponseMessages())
 				.globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
 				.globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
-				.globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
-				.additionalModels(typeResolver.resolve(StandardError.class))				
-				.alternateTypeRules(newRule(List.class, ProductDTO.class, ProductControllerOpenAPI.class));
-	}
-	
-	private <T, M, K> AlternateTypeRule newRule(Class<T> returnType, Class<M> modelObject, Class<K> modelObjectOpenApi) {
-		var typeResolver = new TypeResolver();
-		return AlternateTypeRules.newRule(
-				typeResolver.resolve(ResponseEntity.class, typeResolver.resolve(returnType, modelObject)),
-				typeResolver.resolve(modelObjectOpenApi),
-				Ordered.HIGHEST_PRECEDENCE);		
-	}
+				.globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())				
+				.additionalModels(typeResolver.resolve(StandardError.class));
+	}	
 	
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
@@ -79,11 +64,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 	}
 	
 	private List<Response> globalGetResponseMessages() {
-		return Arrays.asList(
-			new ResponseBuilder()					
-				.code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
-				.description("Recurso não possui representação que poderia ser aceita pelo consumidor")
-				.build(),
+		return Arrays.asList(	
 			new ResponseBuilder()
 				.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
 				.description("Erro interno do servidor")				
@@ -97,14 +78,6 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				.code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
 				.description("Requisição inválida (erro do cliente)")
 				.representation(MediaType.APPLICATION_JSON ).apply(builderModelProblema())
-				.build(),
-			new ResponseBuilder()					
-				.code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
-				.description("Recurso não possui representação que poderia ser aceita pelo consumidor")
-				.build(),				
-			new ResponseBuilder()					
-				.code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
-				.description("Requisição recusada porque o corpo está em um formato não suportado")
 				.build(),		
 			new ResponseBuilder()
 				.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
